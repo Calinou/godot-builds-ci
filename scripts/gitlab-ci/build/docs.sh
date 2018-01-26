e9de7af386a1aedef8617baf296c08c3832d9484
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-01-27 00:35:22 +0100

    GitLab CI: Build documentation in HTML format for offline reading

---
+++ scripts/gitlab-ci/build/docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# This build script is licensed under CC0 1.0 Universal:
+# https://creativecommons.org/publicdomain/zero/1.0/
+
+set -euo pipefail
+
+export DIR
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$DIR/../_common.sh"
+
+# Build documentation in HTML format
+make html SPHINXBUILD="$HOME/.local/bin/sphinx-build"
+mv "_build/" "docs/"
+zip -r9 "$ARTIFACTS_DIR/docs/html.zip" "docs/"

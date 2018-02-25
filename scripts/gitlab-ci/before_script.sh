69a0ccd8a9ff13cb40fd59df65774fa9c9050bf3
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-02-25 20:12:09 +0100

    Improve indentation in the GitLab CI "before script"
    [ci skip]

--- scripts/gitlab-ci/before_script.sh
+++ scripts/gitlab-ci/before_script.sh
@@ -13,23 +13,26 @@ source "$DIR/_common.sh"
 
 if [[ -f "/etc/redhat-release" ]]; then
   # Fedora
-  dnf install -y git cmake scons pkgconfig gcc-c++ curl libxml2-devel libX11-devel \
-                      libXcursor-devel libXrandr-devel libXinerama-devel mesa-libGL-devel \
-                      alsa-lib-devel pulseaudio-libs-devel freetype-devel \
-                      openssl-devel libudev-devel mesa-libGLU-devel mingw32-gcc-c++ mingw64-gcc-c++ \
-                      mingw32-winpthreads-static mingw64-winpthreads-static yasm openssh-clients \
-                      wget zip unzip ncurses-compat-libs wine
+  dnf install -y \
+      git cmake scons pkgconfig gcc-c++ curl libxml2-devel libX11-devel \
+      libXcursor-devel libXrandr-devel libXinerama-devel mesa-libGL-devel \
+      alsa-lib-devel pulseaudio-libs-devel freetype-devel \
+      openssl-devel libudev-devel mesa-libGLU-devel mingw32-gcc-c++ mingw64-gcc-c++ \
+      mingw32-winpthreads-static mingw64-winpthreads-static yasm openssh-clients \
+      wget zip unzip ncurses-compat-libs wine
 else
   # Ubuntu
   apt-get update -yqq
   apt-get install -yqq software-properties-common
   add-apt-repository -y ppa:ubuntu-toolchain-r/test
   apt-get update -yqq
-  apt-get install -y git cmake wget zip unzip build-essential scons pkg-config \
-                     libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev \
-                     libglu-dev libasound2-dev libpulse-dev libfreetype6-dev \
-                     libssl-dev libudev-dev libxrandr-dev libxi-dev yasm \
-                     gcc-7 g++-7
+
+  apt-get install -y \
+      git cmake wget zip unzip build-essential scons pkg-config \
+      libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev \
+      libglu-dev libasound2-dev libpulse-dev libfreetype6-dev \
+      libssl-dev libudev-dev libxrandr-dev libxi-dev yasm \
+      gcc-7 g++-7
 fi
 
 git clone --depth=1 "https://github.com/godotengine/godot.git"

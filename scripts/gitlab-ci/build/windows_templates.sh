895f3a1bf806ff1c5c00364ff097b089cc394e64
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-08-15 00:02:23 +0200

    GitLab CI: Add proper 32-bit Windows export template builds
    64-bit Windows export templates are no longer masqueraded as
    32-bit ones.

--- scripts/gitlab-ci/build/windows_templates.sh
+++ scripts/gitlab-ci/build/windows_templates.sh
@@ -9,18 +9,17 @@ export DIR
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$DIR/../_common.sh"
 
+# The target x86 architecture (can be "64" or "32")
+bits="$1"
+
 # Build Windows export templates
 for target in "release_debug" "release"; do
-  scons platform=windows bits=64 tools=no target=$target use_lto=yes \
+  scons platform=windows bits="$bits" tools=no target="$target" use_lto=yes \
         $SCONS_FLAGS
 done
 
 strip bin/godot.windows.*.exe
 
 # Move Windows export templates to the artifacts directory
-# We're short on build times, so pretend 64-bit binaries are 32-bit binaries
-# to avoid errors in the editor's Export dialog
-cp "$GODOT_DIR/bin/godot.windows.opt.debug.64.exe" "$ARTIFACTS_DIR/templates/windows_64_debug.exe"
-mv "$GODOT_DIR/bin/godot.windows.opt.debug.64.exe" "$ARTIFACTS_DIR/templates/windows_32_debug.exe"
-cp "$GODOT_DIR/bin/godot.windows.opt.64.exe" "$ARTIFACTS_DIR/templates/windows_64_release.exe"
-mv "$GODOT_DIR/bin/godot.windows.opt.64.exe" "$ARTIFACTS_DIR/templates/windows_32_release.exe"
+cp "$GODOT_DIR/bin/godot.windows.opt.debug.$bits.exe" "$ARTIFACTS_DIR/templates/windows_${bits}_debug.exe"
+cp "$GODOT_DIR/bin/godot.windows.opt.$bits.exe" "$ARTIFACTS_DIR/templates/windows_${bits}_release.exe"

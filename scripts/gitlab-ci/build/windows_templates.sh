9485678f3db52885afbb893af7d305453d0e9280
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-09-17 13:51:23 +0200

    Build debug and release Windows export templates in separate jobs

--- scripts/gitlab-ci/build/windows_templates.sh
+++ scripts/gitlab-ci/build/windows_templates.sh
@@ -9,17 +9,17 @@ export DIR
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$DIR/../_common.sh"
 
+# The target type ("debug" or "release")
+target="$1"
+
 # The target x86 architecture (can be "64" or "32")
-bits="$1"
+bits="$2"
 
 # Build Windows export templates
-for target in "release_debug" "release"; do
-  scons platform=windows bits="$bits" tools=no target="$target" use_lto=yes \
-        $SCONS_FLAGS
-done
+scons platform=windows bits="$bits" tools=no target="$target" use_lto=yes \
+      $SCONS_FLAGS
 
 strip bin/godot.windows.*.exe
 
 # Move Windows export templates to the artifacts directory
-cp "$GODOT_DIR/bin/godot.windows.opt.debug.$bits.exe" "$ARTIFACTS_DIR/templates/windows_${bits}_debug.exe"
-cp "$GODOT_DIR/bin/godot.windows.opt.$bits.exe" "$ARTIFACTS_DIR/templates/windows_${bits}_release.exe"
+cp "$GODOT_DIR/bin"/godot.windows.*.exe "$ARTIFACTS_DIR/templates/windows_${bits}_${target}.exe"

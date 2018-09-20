38fe33184e85293bad33e2f9b862b9e514a1ea88
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-09-20 17:21:28 +0200

    Try building 32-bit Windows export templates with LTO enabled again

--- scripts/gitlab-ci/build/windows_templates.sh
+++ scripts/gitlab-ci/build/windows_templates.sh
@@ -21,14 +21,8 @@ fi
 # The target x86 architecture (can be "64" or "32")
 bits="$2"
 
-if [[ "$bits" == "64" ]]; then
-  lto="yes"
-else
-  lto="no"
-fi
-
 # Build Windows export templates
-scons platform=windows bits="$bits" tools=no target="$scons_target" use_lto="$lto" \
+scons platform=windows bits="$bits" tools=no target="$scons_target" use_lto=yes \
       $SCONS_FLAGS
 
 strip bin/godot.windows.*.exe

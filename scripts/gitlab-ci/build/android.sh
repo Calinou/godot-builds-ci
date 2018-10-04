4a9e0351c1a23a69330e2b70f2f412a392f0dfef
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-10-04 14:36:48 +0200

    Update Android SDK and Android NDK to r18

--- scripts/gitlab-ci/build/android.sh
+++ scripts/gitlab-ci/build/android.sh
@@ -24,18 +24,19 @@ dnf install -y java-1.8.0-openjdk-devel
 # Install the Android SDK
 mkdir -p "$CI_PROJECT_DIR/android/"
 cd "$CI_PROJECT_DIR/android/"
-wget -q "https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip"
-unzip -q "sdk-tools-linux-3859397.zip"
-rm "sdk-tools-linux-3859397.zip"
+wget -q "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
+unzip -q ./*.zip
+rm ./*.zip
 mkdir -p "licenses/"
 cp "$CI_PROJECT_DIR/resources/android-sdk-license" "licenses/"
 export ANDROID_HOME="$CI_PROJECT_DIR/android"
 
 # Install the Android NDK
-wget -q "https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip"
-unzip -q "android-ndk-r16b-linux-x86_64.zip"
-rm "android-ndk-r16b-linux-x86_64.zip"
-export ANDROID_NDK_ROOT="$ANDROID_HOME/android-ndk-r16b"
+wget -q "https://dl.google.com/android/repository/android-ndk-r18-linux-x86_64.zip"
+unzip -q ./*.zip
+rm ./*.zip
+mv ./*ndk* ndk/
+export ANDROID_NDK_ROOT="$ANDROID_HOME/ndk"
 cd "$GODOT_DIR/"
 
 # Build Android export template

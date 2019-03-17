0195934787d6668d0d53ff08f8863c4f765de54c
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-03-17 15:07:46 +0100

    Add HTML documentation builds for offline reading

---
+++ scripts/gitlab-ci/before_script_docs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# This build script is licensed under CC0 1.0 Universal:
+# https://creativecommons.org/publicdomain/zero/1.0/
+
+set -euo pipefail
+IFS=$'\n\t'
+
+export DIR
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$DIR/_common.sh"
+
+# Install dependencies and prepare stuff before building
+
+# Documentation is always built on Fedora
+dnf install -yq \
+    git wget zip unzip python3-pip make curl texlive-latex-bin \
+    texlive-dvipng-bin which
+
+pip3 install --user sphinx sphinx_rtd_theme
+
+git clone --depth=1 "https://github.com/godotengine/godot-docs.git"
+mkdir -p "$ARTIFACTS_DIR/docs"
+
+# Print information about the commit to build
+printf -- "-%.0s" {0..72}
+echo ""
+git -C "$CI_PROJECT_DIR/godot-docs/" log --max-count 1
+printf -- "-%.0s" {0..72}
+echo ""
---
+++ scripts/gitlab-ci/build/docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# This build script is licensed under CC0 1.0 Universal:
+# https://creativecommons.org/publicdomain/zero/1.0/
+
+set -euo pipefail
+
+export DIR
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$DIR/../_common.sh"
+
+# Build documentation in HTML format
+make html SPHINXBUILD="$HOME/.local/bin/sphinx-build"
+mv "_build/html/" "godot-docs/"
+zip -r9 "$ARTIFACTS_DIR/docs/godot-docs-html-nightly.zip" "godot-docs/"

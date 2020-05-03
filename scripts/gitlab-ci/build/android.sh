fedf28efd4debd07af62e722aad588dee585d30b
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-05-04 00:01:36 +0200

    Build the `3.2` branch in parallel to `master`
    Azure Pipelines no longer provides macOS 10.13 images, so we have to
    migrate to macOS 10.14.

--- scripts/gitlab-ci/build/android.sh
+++ scripts/gitlab-ci/build/android.sh
@@ -54,9 +54,22 @@ done
 
 cd "$GODOT_DIR/platform/android/java/"
 
-# The `3.1` (stable) branch uses an older method to build Android export templates.
-# Only an APK is generated in this branch (no AAR).
-./gradlew build
+# `master` or `vulkan` branch
+# Create an APK
+./gradlew generateGodotTemplates
+
+# Move the generated Android source ZIP (for the new exporting method).
+# This one will contain only one AAR depending on the current target,
+# so we have to copy the current AAR to a temporary location for merging
+# in the `deploy` job.
+mv "$GODOT_DIR/bin/android_source.zip" \
+  "$ARTIFACTS_DIR/templates/"
+
+# Move the generated Android AAR to a temporary location, so that both AARs
+# can be added to the final `android_source.zip`
+mkdir -p "$ARTIFACTS_DIR/libs/$target/"
+mv "$GODOT_DIR/platform/android/java/app/libs/$target/godot-lib.$target.aar" \
+    "$ARTIFACTS_DIR/libs/$target/"
 
 # Move the generated APK to the artifacts directory
 mv "$GODOT_DIR/bin/android_$target.apk" \

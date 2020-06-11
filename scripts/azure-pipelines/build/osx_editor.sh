08ab8d449b58132fc32378c5e4ed1e132993962b
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-06-11 22:02:24 +0200

    Use hdiutil directly to create macOS editor DMG images
    This should work better on modern macOS versions, in addition to
    removing an external dependency.

--- scripts/azure-pipelines/build/osx_editor.sh
+++ scripts/azure-pipelines/build/osx_editor.sh
@@ -16,19 +16,10 @@ scons platform=osx tools=yes target=debug \
       "${SCONS_FLAGS[@]}"
 
 # Create macOS editor DMG image
-mkdir -p "godot_dmg/"
-cp -r "misc/dist/osx_tools.app/" "godot_dmg/Godot.app/"
-mkdir -p "godot_dmg/Godot.app/Contents/MacOS/"
-cp "bin/godot.osx.tools.64" "godot_dmg/Godot.app/Contents/MacOS/Godot"
-git clone "https://github.com/andreyvit/create-dmg.git" --depth=1
-(
-  cd "create-dmg/"
-  ./create-dmg \
-      --volname "Godot" \
-      --volicon "../godot_dmg/Godot.app/Contents/Resources/Godot.icns" \
-      --hide-extension "Godot.app" \
-      "$BUILD_ARTIFACTSTAGINGDIRECTORY/editor/godot-macos-nightly-x86_64.dmg" \
-      "../godot_dmg/"
-)
+mkdir -p godot_dmg/
+cp -r misc/dist/osx_tools.app/ godot_dmg/Godot.app/
+mkdir -p godot_dmg/Godot.app/Contents/MacOS/
+cp bin/godot.osx.tools.64 godot_dmg/Godot.app/Contents/MacOS/Godot
+hdiutil create -volname Godot -srcfolder godot_dmg/Godot.app/ -ov "$BUILD_ARTIFACTSTAGINGDIRECTORY/editor/godot-macos-nightly-x86_64.dmg"
 
 make_manifest "$BUILD_ARTIFACTSTAGINGDIRECTORY/editor/godot-macos-nightly-x86_64.dmg"

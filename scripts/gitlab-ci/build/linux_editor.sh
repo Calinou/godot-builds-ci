78c0ff54fd2c23dc62d234ad3452e4792ee0c100
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-09-04 14:50:43 +0200

    Build Linux binaries directly in ZIP archives instead of using AppImage
    Godot already statically links all the dependencies not present
    on a base Linux distribution, making AppImage not very useful here.

--- scripts/gitlab-ci/build/linux_editor.sh
+++ scripts/gitlab-ci/build/linux_editor.sh
@@ -20,20 +20,10 @@ scons platform=x11 tools=yes target=release_debug \
       udev=yes use_static_cpp=yes \
       CC="gcc-9" CXX="g++-9" "${SCONS_FLAGS[@]}"
 
-# Create Linux editor AppImage
-strip "bin/godot.x11.opt.tools.64"
-mkdir -p "appdir/usr/bin/" "appdir/usr/share/icons/hicolor/scalable/apps/"
-cp "bin/godot.x11.opt.tools.64" "appdir/usr/bin/godot"
-cp "misc/dist/linux/org.godotengine.Godot.desktop" "appdir/godot.desktop"
-cp "icon.svg" "appdir/usr/share/icons/hicolor/scalable/apps/godot.svg"
-curl -fsSLO "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod +x "linuxdeployqt-continuous-x86_64.AppImage"
-./linuxdeployqt-continuous-x86_64.AppImage \
-    --appimage-extract-and-run \
-    "appdir/godot.desktop" -appimage
+# Create Linux editor ZIP archive.
+cd "$GODOT_DIR/bin/"
+strip "godot.x11.opt.tools.64"
+mv "godot.x11.opt.tools.64" "godot"
+zip -r9 "$ARTIFACTS_DIR/editor/godot-linux-nightly-x86_64.zip" "godot"
 
-mv \
-    "Godot_Engine-"*"-x86_64.AppImage" \
-    "$ARTIFACTS_DIR/editor/godot-linux-nightly-x86_64.AppImage"
-
-make_manifest "$ARTIFACTS_DIR/editor/godot-linux-nightly-x86_64.AppImage"
+make_manifest "$ARTIFACTS_DIR/editor/godot-linux-nightly-x86_64.zip"
